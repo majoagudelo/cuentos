2c64bd380e750f77a0e5c422f8b468e72090f394
majoagudelo <158481201+majoagudelo@users.noreply.github.com>
2024-02-09 09:02:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,142 @@
-let c1= document.getElementById("c1"),
-    c2=document.getElementById("c2"),
-    c3=document.getElementById("c3"),
-    cuento1=document.getElementById("cuento1"),
-    cuento2=document.getElementById("cuento2"),
-    cuento3=document.getElementById("cuento3");
+*{
+    box-sizing: border-box;
+    margin: 0px;
+    padding: 0px;
+}
+
+header{
+    width: 100%;
+    min-height: 50vh;
+    background-image: url(fondo2.jpg);
+    background-position: bottom;
+    background-size: cover;
+    margin: 2px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+body{
+    box-sizing: border-box;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding: 12px;
+    background-image: url(fondo5.jpg);
+     background-size: cover;
+}
+
+h1{
+    text-align: center;
+    font-size:70px ;
+    color: rgb(216, 89, 131);
+    width: 100%;
+}
+
+header p{
+    
+    padding: 12px;
+    text-align: center; 
+    color: rgb(216, 89, 131);
+    width: fit-content;
+
+}
+header div{
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.cuento{
+    max-width: 400px;
+    
+	-webkit-animation: slide-in-elliptic-right-bck 0.7s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+    animation: slide-in-elliptic-right-bck 0.7s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+
+}
 
 
+.imagenes img{
+    width: 300px;
+    border-radius: 12px;
+    margin: 12px;
+}
 
+.contenedor{
+   
+    display: flex;
+    flex-wrap: wrap;
+    margin: 6px;
+    padding: 12px;
+    border:2px solid palevioletred;
+    border-radius: 12px; 
+    background-color: white;
+    
+}
 
-    function mostrar1(){
-        cuento1.style.display="flex"
-        cuento2.style.display="none"
-        cuento3. style.display="none"
+h2{ 
+    color: palevioletred;
+    margin-bottom: 12px;
+}
+
+nav a{
+    background-color: palevioletred;
+    color: white;
+    padding: 6px 12px;
+    margin: 6px;
+    border-radius: 16px;
+}
+
+nav a:hover{
+    background-color: white;
+    color: palevioletred;
+    border: 2px solid palevioletred;
+}
+
+#cuento2{
+ display: none;   
+}
+
+#cuento3{
+    display: none;
+}
+
+#cuento4{
+    display: none;
+}
+@-webkit-keyframes slide-in-elliptic-right-bck {
+    0% {
+      -webkit-transform: translateX(800px) rotateY(30deg) scale(6.5);
+              transform: translateX(800px) rotateY(30deg) scale(6.5);
+      -webkit-transform-origin: -100% 50%;
+              transform-origin: -100% 50%;
+      opacity: 0;
     }
-
-    function mostrar2(){
-        cuento2.style.display="flex"
-        cuento1.style.display="none"
-        cuento3.style.display="none" 
+    100% {
+      -webkit-transform: translateX(0) rotateY(0) scale(1);
+              transform: translateX(0) rotateY(0) scale(1);
+      -webkit-transform-origin: 600px 50%;
+              transform-origin: 600px 50%;
+      opacity: 1;
     }
-
-    function mostrar3(){
-        cuento2.style.display="none"
-        cuento1.style.display="none"
-        cuento3.style.display="flex" 
+  }
+  @keyframes slide-in-elliptic-right-bck {
+    0% {
+      -webkit-transform: translateX(800px) rotateY(30deg) scale(6.5);
+              transform: translateX(800px) rotateY(30deg) scale(6.5);
+      -webkit-transform-origin: -100% 50%;
+              transform-origin: -100% 50%;
+      opacity: 0;
     }
+    100% {
+      -webkit-transform: translateX(0) rotateY(0) scale(1);
+              transform: translateX(0) rotateY(0) scale(1);
+      -webkit-transform-origin: 600px 50%;
+              transform-origin: 600px 50%;
+      opacity: 1;
+    }
+  }
+  
 
-    c1.addEventListener("click" , mostrar1);
-    c2.addEventListener("click", mostrar2);
-    c3.addEventListener("click" , mostrar3);
+
